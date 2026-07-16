b0a4a43fcf5e25b9236c007c9b21888dec84c6dd
hanjian.thu123 <hanjian.thu123@bytedance.com>
2026-07-16 21:54:10 +0800

    [update] add eval_GRN_ind_L.sh

---
+++ scripts/c2i/eval_GRN_ind_L.sh
@@ -0,0 +1,42 @@
+cache_dir=./checkpoints
+batch_size=64
+exp_name=GRN_ind_B
+CKPT_DIR=${cache_dir}/checkpoints/${exp_name}
+generation_dir=${cache_dir}/generation/${exp_name}
+CKPT_FILE='/weights/GRN_ind_L_ep599.pth'
+data_path=./data/imagenet/imagenet_train_val
+vae_path=./weights/HBQ_image_tokenizer_16dim_M4.ckpt
+
+export EXP_NAME=${exp_name}
+export PROJECT="GRN"
+export CKPT_FILE=${CKPT_FILE}
+
+for tau in 1.3
+do
+    for cfg in 2.0
+    do
+        for interval_min in 0.42
+        do
+            torchrun --nproc_per_node=8 --nnodes=1 --node_rank=0 --master_port=29502 \
+            c2i_train_infer.py \
+            --model GRN_L \
+            --proj_dropout 0.0 \
+            --P_mean -0.8 --P_std 0.8 \
+            --img_size 256 --noise_scale 1.0 \
+            --batch_size ${batch_size} --blr 5e-5 \
+            --epochs 600 --warmup_epochs 5 \
+            --gen_bsz ${batch_size} --num_images 50000 --tau ${tau} --cfg ${cfg} --interval_min ${interval_min} --interval_max 1.0 \
+            --output_dir ${CKPT_DIR} \
+            --method GRN_ind \
+            --hbq_round 4 \
+            --in_channels 256 \
+            --vae_path ${vae_path} \
+            --wandb 0 \
+            --data_path ${data_path} \
+            --resume ${CKPT_FILE} \
+            --generation_dir ${generation_dir} \
+            --sampling_method euler \
+            --evaluate_gen
+        done
+    done
+done
